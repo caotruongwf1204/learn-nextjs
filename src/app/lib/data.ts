407fd63e7c2f87e9a01abc306e97d79bdd7fdf2b
caotruongwf1204 <caodinhtruong1204@gmail.com>
2023-12-14 23:07:50 +0700

    up

---
+++ src/app/lib/data.ts
@@ -0,0 +1,34 @@
+import {
+    sql 
+} from '@vercel/postgres';
+import {
+  CustomerField,
+  CustomersTableType,
+  InvoiceForm,
+  InvoicesTable,
+  LatestInvoiceRaw,
+  User,
+  Revenue,
+} from './definitions';
+
+export async function fetchRevenue() {
+    // Add noStore() here prevent the response from being cached.
+    // This is equivalent to in fetch(..., {cache: 'no-store'}).
+    // noStore();
+    try {
+      // Artificially delay a response for demo purposes.
+      // Don't do this in production :)
+  
+      // console.log('Fetching revenue data...');
+      // await new Promise((resolve) => setTimeout(resolve, 3000));
+  
+      const data = await sql<Revenue>`SELECT * FROM revenue`;
+  
+      // console.log('Data fetch completed after 3 seconds.');
+  
+      return data.rows;
+    } catch (error) {
+      console.error('Database Error:', error);
+      throw new Error('Failed to fetch revenue data.');
+    }
+  }
